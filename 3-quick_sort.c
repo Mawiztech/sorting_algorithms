ca3e35a17fc954c1c919c28e3015eabafa95b66f
rachael-ojo <adeolaatilade@gmail.com>
2024-02-20 13:43:51 -0800

    Updated

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,11 +1,11 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swaps two integers.
+ * swap - Swaps two integers.
  * @a: Pointer to the first integer.
  * @b: Pointer to the second integer.
  */
-void swap_ints(int *a, int *b)
+void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
