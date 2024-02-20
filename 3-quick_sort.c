f933316d38315bc3094dad286aa3f17a963ff524
rachael-ojo <adeolaatilade@gmail.com>
2024-02-20 12:42:50 -0800

    Updated

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,10 @@
 #include "sort.h"
 
-/**
+void swap ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int left, int right);
+void lomuto_sort(int *array, size_t size, int left, int right);
+void quick_sort(int *array, size_t size);
+/*
  * swap - Swaps two integers.
  * @a: Pointer to the first integer.
  * @b: Pointer to the second integer.
